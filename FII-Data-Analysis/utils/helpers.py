5494bbda67ddda2b744907816b245b1579c6a624
Eduardo Sartori <115821181+oeduardosartori@users.noreply.github.com>
2024-07-09 17:36:24 -0300

    Release/v1/net worth (#11)
    * feat: project organization
    
    * feat: add method convert-to-float
    
    * feat: organizar menu (#6)
    
    * feat: organizar menu
    
    * fix: update da def print banner
    
    * feat: mvc pattern implementedd (#7)
    
    * feat: add color to data analysis and error return menu (#9)
    
    * fix: add new method converter (#10)
    
    * feat: update analysys
    
    ---------
    
    Co-authored-by: Enrique Sartori <132933322+oenriquesartori@users.noreply.github.com>
    Co-authored-by: SamuelFBrum <134164934+SamuelFBrum@users.noreply.github.com>

--- FII-Data-Analysis/utils/helpers.py
+++ FII-Data-Analysis/utils/helpers.py
@@ -1,14 +1,24 @@
 """
 Módulo utilitário com funções auxiliares para o projeto.
-
-Funções:
-- print_key_value_pairs(data): Imprime os pares chave-valor de um dicionário formatados.
+<<<<<<< HEAD
 """
 
-def print_key_value_pairs(data):
-    for key, value in data.items():
-        print(f"{key}: {value}")
+from colorama import Fore, Style
 
+def print_key_value_pairs(data):
+    color_map = {
+        'Bom': Fore.GREEN,
+        'Razoável': Fore.YELLOW,
+        'Ruim': Fore.RED,
+        'Desconhecido': Fore.LIGHTRED_EX,
+        'Seguro': Fore.GREEN,
+        'Neutro': Fore.YELLOW,
+        'Arriscado': Fore.RED,
+    }
+
+    for key, value in data.items():
+        color = color_map.get(value, Fore.RESET)
+        print(f"{key}: {color}{value}{Style.RESET_ALL}")
 
 def print_banner(msg: str, border: str = '-' * 30):
     print(border)
